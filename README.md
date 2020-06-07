bcedc23df7f182de62fdcdb1a68a671117dfef0b
woojoo <woojoo@softmarshmallow.com>
2020-06-07 18:15:02 +0900

    added documentations

--- README.md
+++ README.md
@@ -2,18 +2,22 @@
 
 modern vocabulary leaning app for SAT/GRE students.
 
+**supported native human languages**
 
-
-**supported native human languages** 
-
-* KOREAN (ko-KR)
-* ENGLISH (en-US)
-
-
+- KOREAN (ko-KR)
+- ENGLISH (en-US)
 
 **project structure**
 
-* /app - word app main folder built on flutter
-* /server - service server built on deno
-* /core. - nlp & logic containers
+- /app - word app main folder built on flutter
+- /server - service server built on deno
+- /core. - nlp & logic containers
 
+## app
+
+![mac-os](./documents/images/readme-mac-os-1.png)
+
+## engine
+
+wor.io uses inked-engin for internal nlp engine processing.
+https://github.com/softmarshmallow/inked-engine
--- server/app.js
+++ server/app.js
@@ -1,16 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./src/routes/index');
-var usersRouter = require('./src/routes/users');
+const indexRouter = require('./src/routes');
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
