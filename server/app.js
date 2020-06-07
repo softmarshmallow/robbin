3b71fd2a660bbe8283785430df92ce0676bbcc23
woojoo <woojoo@softmarshmallow.com>
2020-06-07 17:33:00 +0900

    🕸 added ts support

--- server/app.js
+++ server/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./src/routes/index');
+var usersRouter = require('./src/routes/users');
 
 var app = express();
 
