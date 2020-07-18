2eac3c43656218d72f10c0908b5148c92d0c252d
softmarshmallow <woojoo@softmarshmallow.com>
2020-07-18 18:50:46 +0900

    🕸 removed unused files

--- app/web/firebase-init.js
+++
@@ -1,8 +0,0 @@
-import * as firebaseConfig from "./credentials/firebase.config"
-console.log(firebaseConfig)
-
-importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js");
-importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-auth.js");
-
-firebase.initializeApp(firebaseConfig);
